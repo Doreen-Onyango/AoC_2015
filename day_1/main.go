91b114f644b52be35958246ead6c5b5608f71349
doonyango <datieno001@gmail.com>
2024-08-27 06:03:15 +0300

    feat: implement floor direction

--- day_1/main.go
+++ day_1/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	inst := "(())((())(((((()))))"
+	fmt.Println(FloorDirection(inst))
+}
+
+func FloorDirection(inst string) int {
+	floor := 0
+	up := '('
+	down := ')'
+
+	for _, c := range inst {
+		if c == up {
+			floor += 1
+		} else if c == down {
+			floor -= 1
+		}
+	}
+	return floor
 }
