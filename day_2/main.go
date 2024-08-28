e890c353877f53428fa2605b6b1226e8c586fee7
doonyango <datieno001@gmail.com>
2024-08-28 14:58:23 +0300

    Feat: implement function for perimeter

--- day_2/main.go
+++ day_2/main.go
@@ -12,13 +12,16 @@ func main() {
 	if err != nil {
 		fmt.Println("error reading from file")
 	}
-	sum := 0
+	// sum := 0
+	p := 0
 	newdata := strings.Split(string(data), "\n")
 	for _, ch := range newdata {
-		sum += SurfaceArea(ch)
+		// sum += SurfaceArea(ch)
+		p += Perimeter(ch)
 	}
 
-	fmt.Println(sum)
+	// fmt.Println(sum)
+	fmt.Println(p)
 }
 
 func SurfaceArea(str string) int {
@@ -40,7 +43,7 @@ func SurfaceArea(str string) int {
 		le, _ := strconv.Atoi(l)
 		wi, _ := strconv.Atoi(w)
 		he, _ := strconv.Atoi(h)
-		min := MinArea((le * wi), MinArea((le*he), (wi*he)))
+		min := Min((le * wi), Min((le*he), (wi*he)))
 		res = (2 * le * wi) + (2 * le * he) + (2 * wi * he) + min
 
 	}
@@ -48,9 +51,33 @@ func SurfaceArea(str string) int {
 	return res
 }
 
-func MinArea(x, y int) int {
+func Min(x, y int) int {
 	if x < y {
 		return x
 	}
 	return y
+}
+
+func Perimeter(str string) int {
+	res := 0
+	l, w, h := "", "", ""
+	newstr := strings.Split(str, "x")
+	for i, ch := range newstr {
+		if i == 0 {
+			l = string(ch)
+		}
+		if i == 1 {
+			w = string(ch)
+		}
+		if i == 2 {
+			h = string(ch)
+		}
+		le, _ := strconv.Atoi(l)
+		wi, _ := strconv.Atoi(w)
+		he, _ := strconv.Atoi(h)
+		min := Min((2 * (le + wi)), Min((2*(wi+he)), (2*(le+he))))
+		cube := le * wi * he
+		res = min + cube
+	}
+	return res
 }
