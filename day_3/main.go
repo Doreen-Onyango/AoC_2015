8a3a707b948862885c5dc3c1c1ee6ba68ae65891
doonyango <datieno001@gmail.com>
2024-08-29 17:27:41 +0300

    feat: implement positions - number of houses that deliveries we made atleast once without repetition - road map to the houses

--- day_3/main.go
+++ day_3/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+func main() {
+	data, err := os.ReadFile("puzzle.txt")
+	if err != nil {
+		fmt.Println("error reading from file")
+	}
+	println(Position(string(data)))
+}
+
+func Position(s string) int {
+	count := 1
+	contain := make(map[[2]int]bool)
+	ps := [2]int{0, 0}
+	contain[ps] = true
+	for _, char := range s {
+		if char == '>' {
+			ps[0] += 1
+		}
+		if char == '^' {
+			ps[1] += 1
+		}
+		if char == '<' {
+			ps[0] -= 1
+		}
+		if char == 'v' {
+			ps[1] -= 1
+		}
+
+		if !contain[ps] {
+			count++
+			contain[ps] = true
+		}
+	}
+	return count
 }
