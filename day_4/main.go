b1427a36f59859d0fae05d3b745ced94004d01dd
doonyango <datieno001@gmail.com>
2024-09-01 17:19:46 +0300

    feat: implement md5hash

--- day_4/main.go
+++ day_4/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	skey := "bgvyzdsv"
+	fmt.Println(LowNum(skey))
+
+}
+
+func Hash(str string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(str))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func LowNum(sKey string) int {
+	prefix := "00000"
+	i := 0
+
+	for {
+		input := sKey + strconv.Itoa(i)
+		hash := Hash(input)
+
+		if strings.HasPrefix(hash, prefix) {
+			return i
+		}
+
+		i++
+	}
 }
