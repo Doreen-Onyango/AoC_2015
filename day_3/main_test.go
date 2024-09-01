fb705e74a5600ec14f8430691d5c8f07692de36a
doonyango <datieno001@gmail.com>
2024-09-01 16:34:15 +0300

    Test: implement test for robot santa

--- day_3/main_test.go
+++ day_3/main_test.go
@@ -2,9 +2,18 @@ package main
 
 import "testing"
 
-func TestPresents(t *testing.T) {
+func TestPosition(t *testing.T) {
 	got := Position("^>v<")
 	want := 4
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func TestRoboSanta(t *testing.T) {
+	got := RoboSanta("^v^v^v^v^v")
+	want := 11
 
 	if got != want {
 		t.Errorf("got %v want %v", got, want)
