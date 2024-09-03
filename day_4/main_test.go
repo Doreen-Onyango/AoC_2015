32f1b562cd3e0f3922f17a02298cb0a88d86f475
doonyango <datieno001@gmail.com>
2024-09-03 04:35:01 +0300

    test: implement test for LowNumber

--- day_4/main_test.go
+++ day_4/main_test.go
@@ -9,3 +9,11 @@ func TestLowN(t *testing.T) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
+
+func TestLowNumber(t *testing.T) {
+	got := LowNumber("abcdef")
+	want := 6742839
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
