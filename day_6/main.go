6a6b4fce6252e722ef7575d4f037571b3cd9a367
doonyango <datieno001@gmail.com>
2024-09-17 05:00:10 +0300

    refactor: separate concerns

--- day_6/main.go
+++ day_6/main.go
@@ -7,60 +7,69 @@ import (
 	"strings"
 )
 
-var grid [1000][1000]bool
-
 func main() {
-	ReadInstructions("puzzle.txt")
-	fmt.Println(CountLitLights())
+	grid := make([][]bool, 1000)
+	for i := range grid {
+		grid[i] = make([]bool, 1000)
+	}
+	instructions := ReadInstructions("puzzle.txt")
+
+	for _, instruction := range instructions {
+		Application(grid, instruction)
+	}
+	count := CountLitLights(grid)
+	fmt.Println(count)
 }
 
-func ReadInstructions(filename string) {
+func ReadInstructions(filename string) []string {
+	var instructions []string
 	file, err := os.Open(filename)
 	if err != nil {
-		panic(err)
+		fmt.Println("error opening the file", err)
+		return instructions
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		line := scanner.Text()
+		instructions = append(instructions, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("error reading from file", err)
+	}
+	return instructions
+}
+func Application(grid [][]bool, instruction string) {
+	parts := strings.Split(instruction, " ")
+	var action string
+	var x1, y1, x2, y2 int
 
-		var command string
-		var x1, y1, x2, y2 int
-		fmt.Sscanf(line, "%s %d,%d through %d,%d", &command, &x1, &y1, &x2, &y2)
-
-		switch command {
-		case "turn":
-			if strings.HasPrefix(line, "turn on") {
-				for i := x1; i <= x2; i++ {
-					for j := y1; j <= y2; j++ {
-						grid[i][j] = true
-					}
-				}
-			} else if strings.HasPrefix(line, "turn off") {
-				for i := x1; i <= x2; i++ {
-					for j := y1; j <= y2; j++ {
-						grid[i][j] = false
-					}
-				}
-			}
-		case "toggle":
-			for i := x1; i <= x2; i++ {
-				for j := y1; j <= y2; j++ {
-					grid[i][j] = !grid[i][j]
-				}
+	if parts[0] == "toggle" {
+		action = "toggle"
+		fmt.Sscanf(parts[1], "%d,%d", &x1, &y1)
+		fmt.Sscanf(parts[3], "%d,%d", &x2, &y2)
+	} else {
+		action = parts[1]
+		fmt.Sscanf(parts[2], "%d,%d", &x1, &y1)
+		fmt.Sscanf(parts[4], "%d,%d", &x2, &y2)
+	}
+	for i := x1; i <= x2; i++ {
+		for j := y1; j <= y2; j++ {
+			switch action {
+			case "on":
+				grid[i][j] = true
+			case "off":
+				grid[i][j] = false
+			case "toggle":
+				grid[i][j] = !grid[i][j]
 			}
 		}
 	}
-	if err := scanner.Err(); err != nil {
-		panic(err)
-	}
 }
-
-func CountLitLights() int {
+func CountLitLights(grid [][]bool) int {
 	count := 0
-	for i := 0; i < 1000; i++ {
-		for j := 0; j < 1000; j++ {
+	for i := range grid {
+		for j := range grid[i] {
 			if grid[i][j] {
 				count++
 			}
